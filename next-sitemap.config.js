46d54b058ce83db726ded8470bf24910cea09f42
Afnan Ahmad Tariq <116060169+afnanahmadtariq@users.noreply.github.com>
2025-04-15 02:31:51 +0500

    v1.3.1

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: 'https://yourdomain.com', // Replace with your site's URL
+    siteUrl: 'https://www.devstract.site', // Replace with your site's URL
     generateRobotsTxt: true, // (optional) Generate a robots.txt file
     changefreq: 'daily', // Frequency of changes
     priority: 0.7, // Priority of pages
@@ -7,7 +7,7 @@ module.exports = {
     exclude: ['/admin/*'], // Exclude specific paths
     robotsTxtOptions: {
       additionalSitemaps: [
-        'https://yourdomain.com/sitemap-0.xml', // Add additional sitemaps if needed
+        'https://www.devstract.site/sitemap-0.xml', // Add additional sitemaps if needed
       ],
     },
   }
