dca9f30a3b0f575d9607f1ebfd5248a9c29fbc9e
Afnan Ahmad Tariq <116060169+afnanahmadtariq@users.noreply.github.com>
2025-07-30 19:06:59 +0500

    Update contact-section.tsx

--- components/contact-section.tsx
+++ components/contact-section.tsx
@@ -106,9 +106,9 @@ export default function ContactSection() {
           </div>
 
           {/* Top-right circular arrow with text */}
-          <div className="absolute top-2 right-2 sm:top-6 sm:right-8 z-30">
-            <div className="relative w-16 h-16 sm:w-32 sm:h-32">
-              <svg className="w-16 h-16 sm:w-32 sm:h-32 animate-spin-slow" viewBox="0 0 100 100">
+          <div className="absolute top-2 right-2 sm:top-4 md:top-6 sm:right-4 md:right-8 z-30">
+            <div className="relative w-16 h-16 sm:w-24 md:w-32 sm:h-24 md:h-32">
+              <svg className="w-16 h-16 sm:w-24 md:w-32 sm:h-24 md:h-32 animate-spin-slow" viewBox="0 0 100 100">
                 <defs>
                   <path id="circle" d="M 50, 50 m -30, 0 a 30,30 0 1,1 60,0 a 30,30 0 1,1 -60,0" />
                 </defs>
@@ -117,7 +117,7 @@ export default function ContactSection() {
                 </text>
               </svg>
               <div className="absolute inset-0 flex items-center justify-center">
-                <img src="/media/small_arrow.svg" alt="Submit" className="w-5 h-5 sm:w-10 sm:h-10 transform -rotate-45" style={{ filter: 'brightness(0)' }} />
+                <img src="/media/small_arrow.svg" alt="Submit" className="w-5 h-5 sm:w-8 md:w-10 sm:h-8 md:h-10 transform -rotate-45" style={{ filter: 'brightness(0)' }} />
               </div>
             </div>
           </div>
