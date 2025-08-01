efc070bb74bc0327fb4e0fa3d859ee4a2963dd56
Afnan Ahmad Tariq <116060169+afnanahmadtariq@users.noreply.github.com>
2025-08-01 20:05:36 +0500

    Update page.tsx

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -8,7 +8,7 @@ import Footer from '@/components/footer'
 
 export default function ContactUsPage() {
   const formRef = useRef<HTMLFormElement>(null);
-  const [buttonState, setButtonState] = useState<'idle' | 'sending' | 'success' | 'error'>("error");
+  const [buttonState, setButtonState] = useState<'idle' | 'sending' | 'success' | 'error'>("sending");
   const [result, setResult] = useState<string | null>(null);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -98,8 +98,8 @@ export default function ContactUsPage() {
                   <button 
                     type="submit" 
                     disabled={buttonState === 'sending' || buttonState === 'success' || buttonState === 'error'} 
-                    className="group flex items-center gap-2 p-1 border border-indigo-600 rounded-full text-sm sm:text-base font-medium bg-transparent hover:bg-indigo-50 dark:hover:bg-indigo-900 w-auto min-w-[140px] mx-auto transition-colors"
-                                      >
+                    className="group flex items-center justify-between p-1 border border-indigo-600 rounded-full text-sm sm:text-base font-medium bg-transparent hover:bg-indigo-50 dark:hover:bg-indigo-900 w-auto min-w-[215px] mx-auto transition-colors"
+                  >
                     <span
                       className="px-4 py-2 ml-4 bg-clip-text text-transparent"
                       style={{
@@ -110,8 +110,11 @@ export default function ContactUsPage() {
                         display: 'inline-block',
                       }}
                     >
-                      {buttonState === 'sending' ? 'Sending...' : buttonState === 'success' ? 'Message Sent' : buttonState === 'error' ? 'Sending Failed' : 'Send Message'}
+                      {buttonState === 'sending' ? 'Sending' : buttonState === 'success' ? 'Message Sent' : buttonState === 'error' ? 'Sending Failed' : 'Send Message'}
                     </span>
+                    {buttonState === 'sending' && (
+                      <span className="text-2xl relative -left-6 -top-1" >📨</span>
+                    )}
                     <span
                       className="inline-flex items-center justify-center rounded-full"
                       style={{
